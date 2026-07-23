b9dd54de1be682a924483b50da6d9c22155ba584
Max <maxdassi6@gmail.com>
2026-07-23 01:26:28 +0100

    Correction config/urls

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('game/', include('game.urls')),
     path('lobby/', include('lobby.urls')),
+    path('', RedirectView.as_view(url='/accounts/login/')),
 ]
